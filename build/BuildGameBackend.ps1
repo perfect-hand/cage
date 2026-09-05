c2e8d1ade129b9986a046978dde5e411dee94cb5
Nick Pruehs <npruehs@outlook.com>
2026-09-05 23:29:26 +0200

    Add build script for game backend.

---
+++ build/BuildGameBackend.ps1
@@ -0,0 +1,22 @@
+Import-Module ./GetVersionNumber.psm1
+
+
+Set-Location $PSScriptRoot/../source/Cage.Backend.Game
+
+Write-Output "`nCleaning game backend..."
+dotnet clean
+
+Write-Output "`nBuilding game backend..."
+dotnet build
+
+
+Set-Location $PSScriptRoot/../source/Cage.Backend.Game.Tests
+
+Write-Output "`nCleaning game backend tests..."
+dotnet clean
+
+Write-Output "`nBuilding game backend tests..."
+dotnet build
+
+Write-Output "`nRunning game backend tests..."
+dotnet test
