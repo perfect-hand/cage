8b41397c1a416c6c9edd002822002c1c50881a68
Nick Pruehs <npruehs@outlook.com>
2026-09-05 23:16:56 +0200

    Add tests for game backend.

---
+++ source/Cage.Backend.Game.Tests/MatchEndpointTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Cage.Backend.Game.Tests;
+
+public class MatchEndpointTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly HttpClient httpClient;
+
+    public MatchEndpointTests(WebApplicationFactory<Program> factory)
+    {
+        httpClient = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task CreatesMatch()
+    {
+        var response = await httpClient.PostAsync("/matches", null);
+
+        Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+
+        var match = await response.Content.ReadFromJsonAsync<MatchDto>();
+
+        Assert.NotNull(match);
+        Assert.NotNull(match.Id);
+    }
+}
