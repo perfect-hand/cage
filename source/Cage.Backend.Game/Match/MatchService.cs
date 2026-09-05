3989054fd874c66b19db2a1d3bbf9f1dbcfb20d6
Nick Pruehs <npruehs@outlook.com>
2026-09-05 22:41:31 +0200

    Add logging to game backend.

--- source/Cage.Backend.Game/Match/MatchService.cs
+++ source/Cage.Backend.Game/Match/MatchService.cs
@@ -1,12 +1,14 @@
 namespace Cage.Backend.Game.Match;
 
-public class MatchService
+public class MatchService(ILogger<MatchService> logger)
 {
     internal MatchDto CreateMatch()
     {
-        return new MatchDto
+        var match = new MatchDto
         {
             Id = Guid.NewGuid().ToString()
         };
+        logger.LogInformation("Match {} created.", match.Id);
+        return match;
     }
 }
