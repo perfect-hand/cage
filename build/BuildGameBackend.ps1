baefb6fdb9f6f1c822f9546a06101f7c3a17236a
Nick Pruehs <npruehs@outlook.com>
2026-09-16 00:01:35 +0200

    Deploy game backend to Azure Container App in GitHub workflow.

--- build/BuildGameBackend.ps1
+++ build/BuildGameBackend.ps1
@@ -29,11 +29,11 @@ Set-Location $PSScriptRoot/../source/Cage.Backend.Game
 Write-Output "`nPublishing game backend..."
 dotnet publish -p:Version=$VersionNumber
 
-Write-Output "`nBuilding docker image..."
+Write-Output "`nBuilding game backend docker image..."
 Set-Location $PSScriptRoot/../source/Cage.Backend.Game
 docker build --build-arg version=$VersionNumber -t ghcr.io/perfect-hand/cage-backend-game:$VersionNumber .
 
-Write-Output "`Pushing docker image..."
+Write-Output "`Pushing game backend docker image..."
 $env:GITHUB_TOKEN | docker login ghcr.io -u $env:GITHUB_USER --password-stdin
 docker push ghcr.io/perfect-hand/cage-backend-game:$VersionNumber
 
---
+++ build/DeployGameBackend.ps1
@@ -0,0 +1,18 @@
+Import-Module $PSScriptRoot/GetVersionNumber.psm1
+
+$VersionNumber = Get-VersionNumber
+
+Write-Output "`Deploying game backend to Azure Container App..."
+
+az login `
+    --service-principal `
+    --username 281d9078-5563-4d6d-9741-8a2275a254b4 `
+    --password $env:AZURE_CLIENT_SECRET `
+    --tenant 0573377a-7932-4757-bce2-25fe85980a29
+
+az account set --subscription 2f314f3f-f0a7-4227-a8fd-d21027d83b77
+
+az containerapp update `
+    --name cageeditor-dev-ca `
+    --resource-group cageeditor-dev-rg `
+    --image ghcr.io/perfect-hand/cage-backend-game:$VersionNumber
