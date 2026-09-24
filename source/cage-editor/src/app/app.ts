35c72cfafa01d12404cc9a0683d3a19cea685c1b
Nick Pruehs <npruehs@outlook.com>
2026-09-24 23:08:31 +0200

    Upgrade from Angular version 21 to 22.

--- source/cage-editor/src/app/app.ts
+++ source/cage-editor/src/app/app.ts
@@ -1,13 +1,14 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, ChangeDetectionStrategy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import {Login} from './login/login';
+import { Login } from './login/login';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, Login],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  changeDetection: ChangeDetectionStrategy.Eager,
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('cage-editor');
--- source/cage-editor/src/app/login/login.ts
+++ source/cage-editor/src/app/login/login.ts
@@ -1,17 +1,31 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, signal, computed, OnInit, inject } from '@angular/core';
-import { PublicClientApplication, LogLevel, Configuration, AuthenticationResult, AccountInfo, InteractionRequiredAuthError } from '@azure/msal-browser';
+import {
+  Component,
+  signal,
+  computed,
+  OnInit,
+  inject,
+  ChangeDetectionStrategy,
+} from '@angular/core';
+import {
+  PublicClientApplication,
+  LogLevel,
+  Configuration,
+  AuthenticationResult,
+  AccountInfo,
+  InteractionRequiredAuthError,
+} from '@azure/msal-browser';
 
 @Component({
   selector: 'login',
   imports: [],
   templateUrl: './login.html',
+  changeDetection: ChangeDetectionStrategy.Eager,
   styleUrl: './login.scss',
 })
 export class Login implements OnInit {
-
   private msalInstance!: PublicClientApplication;
-  private accountId = "";
+  private accountId = '';
 
   private http = inject(HttpClient);
 
@@ -20,14 +34,14 @@ export class Login implements OnInit {
 
   ngOnInit() {
     /**
-    * Configuration object to be passed to MSAL instance on creation. 
-    * For a full list of MSAL.js configuration parameters, visit:
-    * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/configuration.md 
-    */
+     * Configuration object to be passed to MSAL instance on creation.
+     * For a full list of MSAL.js configuration parameters, visit:
+     * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/configuration.md
+     */
     const msalConfig: Configuration = {
       auth: {
-        clientId: "7b47f7a5-835d-4623-90b5-9d05ee36c6fe", // Frontend application client ID
-        authority: "https://perfecthandcage.ciamlogin.com/81875ece-550a-4647-8561-ef29633bfe62",
+        clientId: '7b47f7a5-835d-4623-90b5-9d05ee36c6fe', // Frontend application client ID
+        authority: 'https://perfecthandcage.ciamlogin.com/81875ece-550a-4647-8561-ef29633bfe62',
         redirectUri: '/', // You must register this URI on App Registration. Defaults to window.location.href e.g. http://localhost:3000/
       },
       cache: {
@@ -61,10 +75,11 @@ export class Login implements OnInit {
     this.msalInstance = new PublicClientApplication(msalConfig);
     this.msalInstance.initialize().then(() => {
       // Redirect: once login is successful and redirects with tokens, call Graph API
-      this.msalInstance.handleRedirectPromise()
-        .then(result => this.handleAuthenticationResult(result))
-        .catch(error => console.error(error));
-    })
+      this.msalInstance
+        .handleRedirectPromise()
+        .then((result) => this.handleAuthenticationResult(result))
+        .catch((error) => console.error(error));
+    });
   }
 
   private handleAuthenticationResult(result: AuthenticationResult | null) {
@@ -79,9 +94,9 @@ export class Login implements OnInit {
 
   private selectAccount() {
     /**
-    * See here for more info on account retrieval: 
-    * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-common/docs/Accounts.md
-    */
+     * See here for more info on account retrieval:
+     * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-common/docs/Accounts.md
+     */
     const currentAccounts = this.msalInstance.getAllAccounts();
 
     if (!currentAccounts || currentAccounts.length < 1) {
@@ -93,53 +108,58 @@ export class Login implements OnInit {
 
   private showWelcomeMessage(account: AccountInfo) {
     this.userName.set(account.username);
-  };
+  }
 
   protected async signIn() {
     /**
-    * Scopes you add here will be prompted for user consent during sign-in.
-    * By default, MSAL.js will add OIDC scopes (openid, profile, email) to any login request.
-    * For more information about OIDC scopes, visit: 
-    * https://learn.microsoft.com/entra/identity-platform/permissions-consent-overview#openid-connect-scopes
-    */
+     * Scopes you add here will be prompted for user consent during sign-in.
+     * By default, MSAL.js will add OIDC scopes (openid, profile, email) to any login request.
+     * For more information about OIDC scopes, visit:
+     * https://learn.microsoft.com/entra/identity-platform/permissions-consent-overview#openid-connect-scopes
+     */
     const loginRequest = {
-      scopes: ["User.Read"],
+      scopes: ['User.Read'],
     };
 
-    await this.msalInstance.loginRedirect(loginRequest)
+    await this.msalInstance.loginRedirect(loginRequest);
   }
 
   protected async signOut() {
     const currentAccount = this.msalInstance.getAccount({ homeAccountId: this.accountId });
 
     await this.msalInstance.logoutRedirect({
-      account: currentAccount
+      account: currentAccount,
     });
   }
 
   protected async sendRequest() {
     var request = {
-      scopes: ["api://a6791fa3-10da-4339-8097-ba31b7245e02/user_impersonation"],
+      scopes: ['api://a6791fa3-10da-4339-8097-ba31b7245e02/user_impersonation'],
     };
 
-    this.msalInstance.acquireTokenSilent(request).then(tokenResponse => {
-      console.log(tokenResponse);
+    this.msalInstance
+      .acquireTokenSilent(request)
+      .then((tokenResponse) => {
+        console.log(tokenResponse);
 
-      this.http.get('https://cage-simulation.azurewebsites.net/api/HttpExample', {
-        headers: {
-          'Authorization': "Bearer " + tokenResponse.accessToken,
-        },
-        responseType: 'text'
-      }).subscribe((response) => {
-        console.log(response);
+        this.http
+          .get('https://cage-simulation.azurewebsites.net/api/HttpExample', {
+            headers: {
+              Authorization: 'Bearer ' + tokenResponse.accessToken,
+            },
+            responseType: 'text',
+          })
+          .subscribe((response) => {
+            console.log(response);
+          });
+      })
+      .catch((error) => {
+        if (error instanceof InteractionRequiredAuthError) {
+          // fallback to interaction when silent call fails
+          this.msalInstance.acquireTokenRedirect(request);
+        }
+
+        // handle other errors
       });
-    }).catch(error => {
-      if (error instanceof InteractionRequiredAuthError) {
-        // fallback to interaction when silent call fails
-        this.msalInstance.acquireTokenRedirect(request)
-      }
-
-      // handle other errors
-    });
   }
 }
